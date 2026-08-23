7100bc86f98a4f81cc99bb1ccf914d2d88f0dc88
Fauwaz Alhassan <123549511+AlhassanFauwaz@users.noreply.github.com>
2026-08-23 12:57:36 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,10 @@ import { startDeadlineScheduler } from "./jobs/deadlineScheduler.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'https://fauz-scholarship-alert-1-ghxp.onrender.com',
+    credentials: true,
+  }));
 app.use(express.json());
 app.use(morgan("dev"));
 
